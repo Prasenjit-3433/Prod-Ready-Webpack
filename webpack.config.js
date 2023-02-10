0ffa103415876a39272a2e52dfbeca647e427690
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-02-10 21:04:24 +0530

    customizing generated HTML files

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,12 @@ module.exports = {
             filename: 'styles.[contenthash].css'
         }),
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            title: "Hello World!",
+            filename: 'subfolder/custom_filename.html',
+            meta: {
+                description: 'Some Description'
+            }
+        })
     ]
 };
