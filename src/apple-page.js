5e401ea3516fd928ab2172ecd99ea3b4c6a0162c
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-02-14 21:14:04 +0530

    Extracting Common Dependencies While Code Splitting

--- src/apple-page.js
+++ src/apple-page.js
@@ -1,8 +1,9 @@
 import Heading from './components/heading/heading';
 import AppleImage from './components/apple-image/apple-image';
+import _ from 'lodash';
 
 const heading = new Heading();
-heading.render();
+heading.render(_.upperFirst('apple'));
 
 const appleImage = new AppleImage();
 appleImage.render();
