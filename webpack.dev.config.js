0812153ded50b3bbcd4552029479e3a5a6eff5fe
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-02-14 23:56:46 +0530

    setting dev env

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,9 +4,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'hello-world': './src/hellow-world-page.js',
+        'apple': './src/apple-page.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, './dist'),
         publicPath: ''
     },
@@ -77,9 +80,18 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
+            filename: 'hello-world.html',
+            chunks: ['hello-world'],
             title: "Hello World!",
-            template: 'src/index.hbs',
-            description: 'Create from template'
+            template: 'src/page-template.hbs',
+            description: 'Hello World Page'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'apple.html',
+            chunks: ['apple'],
+            title: "Apple!",
+            template: 'src/page-template.hbs',
+            description: 'Apple Page'
         })
     ]
 };
