f62cb24e9a9551aa799a14fba875e90b88d24c63
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-02-15 12:53:26 +0530

    handing multiple pages

--- src/apple-page.js
+++ src/apple-page.js
@@ -1,6 +1,5 @@
 import Heading from './components/heading/heading';
 import AppleImage from './components/apple-image/apple-image';
-import React from 'react';
 
 const heading = new Heading();
 heading.render('apple');
--- src/server.js
+++ src/server.js
@@ -3,10 +3,19 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
+// Serving Static files
 app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
-app.get('/', function(req, res) {
+// Route for hello-world page:
+app.get('/hello-world', function(req, res) {
     const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
+})
+
+// Route for apple page:
+app.get('/apple', function(req, res) {
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/apple.html');
     const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
     res.send(contentFromHtmlFile);
 })
